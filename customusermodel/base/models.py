0d4ba4f55e1e27d5bebb83e336b975a770e472f1
sunhine526 <38558447+sunhine526@users.noreply.github.com>
2022-08-03 20:08:38 +0300

    finished
    Django project .
    install python venv venvname
    then go to venv name install django
    then python mange.py runserver

---
+++ customusermodel/base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+ name = models.CharField(max_length=200, null=True)
+ email = models.CharField(max_length=500,unique=True)
+ bio = models.TextField(null=True)
+
+
+ USERNAME_FIELD = 'email'
+ REQUIRED_FIELDS = []
